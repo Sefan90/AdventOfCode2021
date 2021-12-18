840290db925f49c18b06398e51d8f293f8f58f99
Stefan <stevetv90@gmail.com>
2021-12-18 12:25:16 +0100

    Day 14 & 15

--- Day14/Part2.py
+++ Day14/Part2.py
@@ -1,12 +1,34 @@
-#def rec():
+import copy
+data = open("input.txt").read().split("\n")
 
+dct = {line.split(" -> ")[0]:line.split(" -> ")[1] for line in data if "->" in line}
+sum_dct = {line.split(" -> ")[0]:0 for line in data if "->" in line}
+word = data[0]
+last = word[-1]
+checklist = list(set(line.split("-> ")[1] for line in data if "->" in line))
 
-def part2():
-    input = open('input.txt').readlines()
-    template = input[0].strip()
-    rules = {i.split("->")[0].strip():i.split("->")[1].strip() for i in input if "->" in i}
-    rules2 = {i.split("->")[0].strip():"" for i in input if "->" in i}
-    for k in rules.keys():
-        rules2[k] += rules[k[0]+rules[k]]+rules[rules[k]+k[1]]
-    print(rules2)
-part2()
+for k in dct:
+    if k in word:
+        sum_dct[k] += 1
+
+for i in range(40):
+    tmp_dct = copy.deepcopy(sum_dct)
+    for k, v in dct.items():
+        tmp = sum_dct.get(k)
+        if tmp > 0:
+            tmp_dct[k[0] + v] += tmp
+            tmp_dct[v + k[1]] += tmp
+            tmp_dct[k] -= tmp
+    sum_dct = tmp_dct   
+
+sum_list = list(set(line.split(" -> ")[1] for line in data if "->" in line))
+resultat_lista = []
+for x in sum_list:
+    tmp_sum = 0
+    for k, v in sum_dct.items():
+        if x in k[0]:
+            tmp_sum += v
+    if last == x:
+        tmp_sum += 1
+    resultat_lista.append(tmp_sum)
+print(max(resultat_lista) - min(resultat_lista))
